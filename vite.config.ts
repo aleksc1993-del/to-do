bea6caf7cf7e59178588ac42ab5bd6a6d910d185
Alexandr93 <Aleksc1993@gmail.com>
2026-08-02 23:07:08 +0300

    fix hot reload

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,11 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    watch: {
+      // Надёжно отслеживаем изменения файлов в Windows и виртуальных FS.
+      usePolling: true,
+      interval: 100,
+    },
+  },
 });
